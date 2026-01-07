9011607fd7cdd898f7b5e45528e3324fafea1abb
krishnadeepika08 <krishnadeepikakurella@gmail.com>
2026-01-07 15:48:58 +0530

    Add .gitignore and MIT LICENSE

---
+++ netflix-gpt/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  
+
+  return (
+    
+      <div>
+   <p className="text-center text-3xl text-amber-200 my-10 font-bold">hello world</p>
+      </div>
+
+    
+  )
+}
+
+export default App
